e93aa96a1b626de730ac1da0e19bb2117bfee967
yunzexie <yux028@ucsd.edu>
2023-05-06 19:47:34 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,28 +3,40 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // Get a reference to the image element
-  let image = document.querySelectorAll('img');
-  let horn_image = image[0];
-
-  // Get a reference to the select element
+  // Option to image and audio
+  const image = document.querySelectorAll('img');
+  const horn_image = image[0];
   const select = document.querySelector("#horn-select");
+  const audio = document.querySelector('.hidden');
+  // Click and play
+  const button = document.querySelector("button");
 
-  // Add an event listener to the select element that changes the image source when an option is selected
+
+
+
+
   select.addEventListener("change", () => {
     if (select.value == 'air-horn') {
       horn_image.src = `assets/images/air-horn.svg`;
       horn_image.alt = `Air horn selected`;
+      audio.src = "assets/audio/air-horn.mp3";
     } else if (select.value == 'car-horn') {
       horn_image.src = `assets/images/car-horn.svg`;
       horn_image.alt = `Car horn selected`;
+      audio.src = "assets/audio/car-horn.mp3";
     } else if (select.value == 'party-horn') {
       horn_image.src = `assets/images/party-horn.svg`;
       horn_image.alt = `Party horn selected`;
+      audio.src = "assets/audio/party-horn.mp3";
     } else {
+      audio.src = "";
       return;
     }
-    
+  });
+
+  button.addEventListener("click", () => {
+    audio.currentTime = 0;
+    audio.play();
   });
 
 }
