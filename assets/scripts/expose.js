7724b75f03fc9132d22bff2b07b760781ea11684
yunzexie <yux028@ucsd.edu>
2023-05-06 19:18:32 -0700

    Update expose.js

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,28 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  // Get a reference to the image element
+  let image = document.querySelectorAll('img');
+  let horn_image = image[0];
+
+  // Get a reference to the select element
+  const select = document.querySelector("#horn-select");
+
+  // Add an event listener to the select element that changes the image source when an option is selected
+  select.addEventListener("change", () => {
+    if (select.value == 'air-horn') {
+      horn_image.src = `assets/images/air-horn.svg`;
+      horn_image.alt = `Air horn selected`;
+    } else if (select.value == 'car-horn') {
+      horn_image.src = `assets/images/car-horn.svg`;
+      horn_image.alt = `Car horn selected`;
+    } else if (select.value == 'party-horn') {
+      horn_image.src = `assets/images/party-horn.svg`;
+      horn_image.alt = `Party horn selected`;
+    } else {
+      return;
+    }
+    
+  });
+
 }
